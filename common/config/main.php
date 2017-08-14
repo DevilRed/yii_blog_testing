0fd39d4e2f6a8d3babe5cd716d340bc365e611fc
Christian <christian.quispe@avantica.net>
2017-08-14 16:37:09 -0400

    added auth config to use DbManager for managing authorization in yii app

--- common/config/main.php
+++ common/config/main.php
@@ -5,6 +5,11 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        // setting up the auth manager for authorization
+        // Yii provides two types of authorization managers: yii\rbac\PhpManager and yii\rbac\DbManager. The former uses a PHP script file to store authorization data, while the latter stores authorization data in a database
+        'authManager' => [
+	        'class' => 'yii\rbac\DbManager',
+        ],
     ],
     'name' => 'My-Yii Blog',// this setting is available under:  Yii::$app->name
 ];
