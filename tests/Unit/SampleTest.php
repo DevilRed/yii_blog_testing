338ebc680cdae2159b1c0a514879dcfd69854b50
Christian <chrisdaq@gmail.com>
2020-04-04 16:02:01 -0400

    got success test

---
+++ tests/Unit/SampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\Unit;
+
+// use yii\phpunit\TestCase;
+use PHPUnit\Framework\TestCase;
+
+class SampleComponentTest extends TestCase {
+
+	public function testSample()
+    {
+        $this->assertTrue(1 == 1);
+    }
+}
