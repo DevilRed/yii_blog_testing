29905a930569d6157104fb617a90e06f8747233f
Christian <chrisdaq@gmail.com>
2020-04-05 10:44:59 -0400

    tweaking phpunit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,13 +3,18 @@
 defined('YII_DEBUG') || define("YII_DEBUG", true);
 defined('YII_ENV') || define("YII_ENV", "test");
 
-getenv('DB_PATH') || putenv("DB_PATH=" . __DIR__ . '/db.sqlite');
 
-require_once(dirname(__DIR__) . '/vendor/autoload.php');
-require_once(dirname(__DIR__) . '/vendor/yiisoft/yii2/Yii.php');
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require __DIR__ .'/../vendor/autoload.php';
 
-/*Yii::setAlias(
-    '@Horat1us/Package/Migrations',
-    dirname(__DIR__) . DIRECTORY_SEPARATOR . "migrations"
-);*/
-// Yii::setAlias('@configFile', __DIR__ . DIRECTORY_SEPARATOR . 'config.php');
+require(__DIR__ . '/../common/config/bootstrap.php');
+require(__DIR__ . '/../frontend/config/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../common/config/main.php'),
+    require(__DIR__ . '/../common/config/main-local.php'),
+    require(__DIR__ . '/../frontend/config/main.php'),
+    require(__DIR__ . '/../frontend/config/main-local.php')
+);
+
+(new yii\web\Application($config));
