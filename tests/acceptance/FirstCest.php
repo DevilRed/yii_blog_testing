5d1bc675eee352d80447ad5075422ff315325abb
Christian <chrisdaq@gmail.com>
2020-08-08 21:12:14 -0400

    acceptance test demo, run using: vendor/bin/codecept run acceptance [fileName], which is in tests/acceptance/ folder

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -1,13 +1,23 @@
-<?php 
+<?php
 
 class FirstCest
 {
-    public function _before(AcceptanceTester $I)
+    public function _before(AcceptanceTester $I)// like setUp()
     {
     }
 
-    // tests
-    public function tryToTest(AcceptanceTester $I)
+    public function frontpageWorks(AcceptanceTester $I)
     {
+    	// https://codeception.com/docs/03-AcceptanceTests
+    	// https://codeception.com/docs/modules/PhpBrowser#Actions
+      $I->amOnPage('/');
+      $I->see('Home');
+    }
+
+    public function brokenLinks(AcceptanceTester $I) {
+    	$I->amOnPage('/');
+      $I->click('About', '#w0');
+      //$I->dontSee("uy");
+      $I->see("Not Found");
     }
 }
